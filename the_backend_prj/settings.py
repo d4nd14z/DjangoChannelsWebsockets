9970767a8fb8102af8783b5b42029caa04d4bb06
d4nd14z <dandiazfo@gmail.com>
2025-02-06 17:50:34 -0500

    Creación de Templates para Room-Chat

--- the_backend_prj/settings.py
+++ the_backend_prj/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',    
-    'rest_framework',
-    'daphne',
+    'rest_framework',    
     'chat',
 
 ]
