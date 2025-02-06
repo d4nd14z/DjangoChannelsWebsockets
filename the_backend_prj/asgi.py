f9bdfa39cdcc9bd4d20a5ff2eabaf0fe1c710252
d4nd14z <dandiazfo@gmail.com>
2025-02-06 17:36:46 -0500

    Configuracion de channels en archivo asgi

--- the_backend_prj/asgi.py
+++ the_backend_prj/asgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_backend_prj.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter (
+    {
+        "http": get_asgi_application(),
+        # únicamente HTTP por ahora... (Luego podremos agregar otros protocolos).
+    }
+)
