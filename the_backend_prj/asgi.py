f3ee74fb183bfe9ce7b568f3de12b3a82d98235c
d4nd14z <dandiazfo@gmail.com>
2025-02-06 18:11:29 -0500

    Se configura el archivo de enrutamiento para las peticiones/requests al WebSocket

--- the_backend_prj/asgi.py
+++ the_backend_prj/asgi.py
@@ -8,14 +8,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_backend_prj.settings')
 
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter (
     {
-        "http": get_asgi_application(),
-        # únicamente HTTP por ahora... (Luego podremos agregar otros protocolos).
+        "http": django_asgi_app(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
